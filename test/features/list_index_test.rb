89ce89933fe9d1c16469d0e7225ebb0c7cad12fa
David Ker <snappy316@gmail.com>
2015-03-17 13:12:12 -0700

    Initial test

---
+++ test/features/list_index_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+feature "Viewing lists" do
+  scenario "homepage shows list index" do
+    visit root_path
+    page.text.must_include "Lists"
+  end
+
+  scenario "one list page has multiple steps" do
+    visit list_path(:one)
+    page.text.must_include "Open fridge"
+  end
+end
